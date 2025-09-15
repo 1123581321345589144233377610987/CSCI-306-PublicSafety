29a65318e2c5476899d91da92ad74b58ba6f81f3
ethanlagrone <ethan_lagrone@mines.edu>
2025-09-15 13:17:57 -0600

    Update BaseballPlayer.java

--- BaseballPlayer.java
+++ BaseballPlayer.java
@@ -1,7 +1,9 @@
 
 public class BaseballPlayer extends Person {
-	String team;
-	String city;
+	private String team;
+	private String city;
+	private final static String [] answers = {"Rockies", "Cardinals", "Yankees", "Blue Jays"};
+	private static int x = 0;
 	
 	public BaseballPlayer(String myName, String occupation, String team, String city) {
 		super(myName, occupation);
@@ -9,14 +11,30 @@ public class BaseballPlayer extends Person {
 		this.city = city;
 	}
 
+	//Override abstract answerQuestion and iterate through answer lists
 	@Override
 	void answerQuestion() {
-		System.out.println("All I really care about is winning for " + this.team);
+		if(x < answers.length-1) {
+			x++;
+		} else {
+			x = 0;
+		}
+		if(this.team == answers[x]) {
+			System.out.println("I like the team I play for, I don't wanna beat the " + this.team);
+		} else {
+			System.out.println("I play for the " + this.team + ". All I really care about is beating the " + answers[x]);
+		}
 	}
-
+	
+	//overriding the abstract func
 	@Override
 	void askQuestion() {
 		System.out.println("Do you live in " + this.city + "?");
 	}
 	
+	//Overriding the whatIDo function for a baseball player
+	@Override
+	public void whatIDo(){
+		System.out.println("I hate fielding grounders, but that's part of the hobby I guess. I don't like to talk about my career.");
+	}
 }
