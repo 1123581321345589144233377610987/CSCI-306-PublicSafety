e51af789aaaa72794e387e10011e2f28f5346c79
1123581321345589144233377610987 <123220105+1123581321345589144233377610987@users.noreply.github.com>
2025-09-14 23:17:35 -0600

    Update Eddie.java
    Eddie can now ask multiple questions sequentially from an array

--- Eddie.java
+++ Eddie.java
@@ -5,12 +5,12 @@ import java.util.Random;
 
 public class Eddie extends Person{
 	private static int counter = 1;
-	private String question ="....Didn't I just ask a question?";
+	private String [] questions ={"What is this for?", "Wait, what do you mean I'm a person?"};
 	private String answer = "Wait... I need to know what this is for";
 	private final static String [] opinions = {"like", "love", "don't like", "hate"};
 	private final static String [] objects = {"the color blue", "pickleball", "card games", "getting up early", "going to bed late"};
 	private String opinion;
-	private boolean answered;
+	private int answered;
 	
 	private int cloneNum=1;
 	private String cloneName;
@@ -21,21 +21,21 @@ public class Eddie extends Person{
 		cloneNum=counter;
 		this.cloneName="Eddie #"+cloneNum;
 		counter++;
-		answered=false;
+		answered=0;
 		opinion= "I really "+opinions[random.nextInt(opinions.length)]+" "+objects[random.nextInt(objects.length)]+".";
 		
 	}
 	public void askQuestion() {
-		System.out.println(question);
+		System.out.println(questions[answered%questions.length]);
+		answered++;
 	}
 	public void answerQuestion() {
-		if (answered) {
+		if (answered>0) {
 			System.out.println("I don't know if I mentioned this, but unlike the other Eddies, "+opinion);
 			System.out.println("Clones are indiviuals too.");
 		}
 		else {
 			System.out.println(answer);
-			answered=true;
 		}
 	}
 
@@ -46,4 +46,3 @@ public class Eddie extends Person{
 	}
 	
 }
-
